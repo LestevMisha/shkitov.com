573367e1391799cea64164c7c74271822b8c8d31
LesMiss <lestev.mi@gmail.com>
2023-06-29 11:30:20 +0800

    new stream (date and telegram-link was changes)

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const targetDate = new Date("Jule 22, 2023"); // Replace "June 22, 2023" with your desired target date
+    const targetDate = new Date("Jule 13, 2023"); // Replace "June 22, 2023" with your desired target date
 
     const isTimed = localStorage.getItem("isTimed");
     if (!isTimed) {
