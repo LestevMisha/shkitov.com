b75a0d90198f668aef2ab7b8c5837b1d922829fa
LesMiss <lestev.mi@gmail.com>
2023-06-29 12:24:08 +0800

    test

--- js/countdown.js
+++ js/countdown.js
@@ -25,30 +25,32 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("seconds_number").innerHTML = default_;
             localStorage.clear();
             clearInterval(countdownInterval);
+        } else {
+
+
+            // Time calculations for days, hours, minutes, and seconds
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Russian names for days, hours, minutes, and seconds
+            const daysString = days === 1 ? "день" : days < 5 ? "дня" : "дней";
+            const hoursString = hours === 1 ? "час" : hours < 5 ? "часа" : "часов";
+            const minutesString = minutes === 1 ? "минута" : minutes < 5 ? "минуты" : "минут";
+            const secondsString = seconds === 1 ? "секунда" : seconds < 5 ? "секунды" : "секунд";
+
+            // Display the result in the element with the corresponding IDs
+            document.getElementById("days_number").innerHTML = days;
+            document.getElementById("hours_number").innerHTML = hours;
+            document.getElementById("minutes_number").innerHTML = minutes;
+            document.getElementById("seconds_number").innerHTML = seconds;
+
+            document.getElementById("days").innerHTML = daysString;
+            document.getElementById("hours").innerHTML = hoursString;
+            document.getElementById("minutes").innerHTML = minutesString;
+            document.getElementById("seconds").innerHTML = secondsString;
         }
-
-        // Time calculations for days, hours, minutes, and seconds
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Russian names for days, hours, minutes, and seconds
-        const daysString = days === 1 ? "день" : days < 5 ? "дня" : "дней";
-        const hoursString = hours === 1 ? "час" : hours < 5 ? "часа" : "часов";
-        const minutesString = minutes === 1 ? "минута" : minutes < 5 ? "минуты" : "минут";
-        const secondsString = seconds === 1 ? "секунда" : seconds < 5 ? "секунды" : "секунд";
-
-        // Display the result in the element with the corresponding IDs
-        document.getElementById("days_number").innerHTML = days;
-        document.getElementById("hours_number").innerHTML = hours;
-        document.getElementById("minutes_number").innerHTML = minutes;
-        document.getElementById("seconds_number").innerHTML = seconds;
-
-        document.getElementById("days").innerHTML = daysString;
-        document.getElementById("hours").innerHTML = hoursString;
-        document.getElementById("minutes").innerHTML = minutesString;
-        document.getElementById("seconds").innerHTML = secondsString;
 
     }, 1000);
 });
