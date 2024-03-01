96b22c0d39be996e15cde8c99b10001c1143bfa0
Misha Lestev <mishalestev@Mishas-MacBook-Pro.local>
2024-03-01 12:43:23 -0600

    march

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const butttons = document.querySelectorAll(".b-btn");
     butttons.forEach((button) => {
         button.addEventListener("click", function () {
-            document.location.href = "https://t.me/+EAAVfS45pIRlMjQ6";
+            document.location.href = "https://t.me/+X9f-EMioOdlkMTJi";
         })
     });
 
